ed375193f3fde73fbf0d7f7e4924ddfd704eeb38
Bennett <bennett@blizzard.com>
2015-06-30 15:14:44 -0700

    chmod package.json, spacing to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,47 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-        less: {
-            development: {
-                options: {
-                    compress: false,
-                    cleancss: false,
-                    optimization: 2,
-                    dumpLineNumbers: 'false'
-                },
-                files: {
-                    "css/skeleton.css": "less/skeleton.less"
-                }
-            }
+
+  grunt.initConfig({
+
+    // Project configuration
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Compiles our Less
+    less: {
+      development: {
+        options: {
+          compress: false,
+          cleancss: false,
+          optimization: 2,
+          dumpLineNumbers: 'false'
         },
-
-        watch: {
-            options: {
-                livereload: false,
-            },
-            styles: {
-                files: ['less/**/*.less'], // which files to watch
-                tasks: ['less'],
-                options: {
-                    nospawn: true
-                }
-            }
+        files: {
+          "css/skeleton.css": "less/skeleton.less"
         }
-    });
+      }
+    },
 
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('default', ['less', 'watch']);
+    // Build tooling
+
+    watch: {
+      options: {
+      	livereload: false,
+      },
+      styles: {
+        files: ['less/**/*.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+
+  });
+
+  // Load dependencies
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Generate and format the CSS
+  grunt.registerTask('default', ['less', 'watch']);
+
 };
